c1fc99647ccdfa1e050e633b3986d3f22f14ea7d
peesasidech <129536390+Skyusehot007@users.noreply.github.com>
2023-09-27 16:56:46 +0700

    Player

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float Speed = 1f;
+    public float sprintSpeed = 5f;
+
+    Vector2 moveMent;
+
+    public Animator animator;
+    private bool isSprinting = false;
+    private StaminaBar staminaBar;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        staminaBar = StaminaBar.instance;
+    }
+
+    void Update()
+    {
+        moveMent.x = Input.GetAxisRaw("Horizontal");
+        moveMent.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", moveMent.x);
+        animator.SetFloat("Vertical", moveMent.y);
+        animator.SetFloat("Speed", moveMent.sqrMagnitude);
+
+
+        if (staminaBar != null)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && staminaBar.currentStamina > 0)
+            {
+                isSprinting = true;
+                staminaBar.UseStamina(1);
+            }
+
+            if (isSprinting && staminaBar.currentStamina > 0)
+            {
+                // ใช้ rb.velocity เพื่อควบคุมเคลื่อนที่
+                rb.velocity = moveMent.normalized * sprintSpeed;
+            }
+            else
+            {
+                rb.velocity = moveMent.normalized * Speed;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                isSprinting = false;
+            }
+        }
+    }
+}
---
+++ Assets/Script/StaminaBar.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider staminaBar;
+    private int maxStamina = 500;
+    public int currentStamina;
+    private WaitForSeconds regenTick = new WaitForSeconds(0.3f);
+    private Coroutine regen;
+    public static StaminaBar instance;
+
+    public Vector3 offset;
+    public Transform player;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = maxStamina;
+    }
+    private void Update()
+    {
+
+        if (player != null)
+        {
+            Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(player.position + offset);
+            staminaBar.transform.position = playerScreenPos;
+        }
+        else
+        {
+            staminaBar.gameObject.SetActive(false);
+        }
+    }
+
+
+    public void UseStamina(int amount)
+    {
+        if (currentStamina - amount >= 0)
+        {
+            currentStamina -= amount;
+            staminaBar.value = currentStamina;
+
+            if (regen != null)
+                StopCoroutine(regen);
+
+            regen = StartCoroutine(RegenStamina());
+        }
+        else
+        {
+            Debug.Log("Not enough stamina");
+        }
+    }
+
+    private IEnumerator RegenStamina()
+    {
+        yield return new WaitForSeconds(2);
+
+        while (currentStamina < maxStamina)
+        {
+            currentStamina += maxStamina / 8;
+            staminaBar.value = currentStamina;
+            yield return regenTick;
+        }
+        regen = null;
+    }
+}
