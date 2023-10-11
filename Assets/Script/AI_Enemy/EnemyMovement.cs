6564dd951da8dc0b453719d1cd4de8c804ad4b86
ivamxx <terracatz19@gmail.com>
2023-10-11 17:28:11 +0700

    Ai

--- Assets/Script/AI_Enemy/EnemyMovement.cs
+++ Assets/Script/AI_Enemy/EnemyMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private Vector3 target;
+    [SerializeField] Transform target;
     NavMeshAgent agent;
 
     private void Awake()
@@ -19,20 +19,6 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
-        SetTargetPosition();
-        SetAgentPosition();
-    }
-
-    void SetTargetPosition()
-    {
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-    }
-
-    void SetAgentPosition() 
-    {
-        agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
+        agent.SetDestination(target.position);
     }
 }
