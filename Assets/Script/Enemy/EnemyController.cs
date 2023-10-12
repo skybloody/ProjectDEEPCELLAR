d641031c2ca1bf03d6ae8b292fad68548fd9d71d
ivamxx <terracatz19@gmail.com>
2023-10-12 17:08:23 +0700

    1

--- Assets/Script/Enemy/EnemyController.cs
+++ Assets/Script/Enemy/EnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFollow : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     public float speed;
     public float minRange;
