00d94d484c171105f8dec6736624f1019faa489b
Suniza3 <146001943+Suniza3@users.noreply.github.com>
2023-10-20 22:20:06 +0700

    NoteScene

--- Assets/Script/Paper.cs
+++ Assets/Script/Paper.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Paper : MonoBehaviour
+public class Note_Paper : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Image _noteImage;
+
+    public GameObject MessagePanel;
+    public bool Action = false;
+
     void Start()
     {
-        
+        MessagePanel.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Action == true)
+            {
+                MessagePanel.SetActive(false);
+                Action = false;
+                _noteImage.enabled = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MessagePanel.SetActive(true);
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MessagePanel.SetActive(false);
+            Action = false;
+            _noteImage.enabled = false;
+        }
     }
 }
