e147177b06debc7b2ccb096d86a2e734dfd5a87a
peesasidech <129536390+Skyusehot007@users.noreply.github.com>
2023-10-29 03:59:11 +0700

    Footstepplayertest

--- Assets/Script/Player/FootstepSystem.cs
+++ Assets/Script/Player/FootstepSystem.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class FootstepSystem : MonoBehaviour
 {
-    private AudioSource audioSource;
-
-    public AudioClip[] footstepSounds;
+    public AudioSource audioSource;
+    
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
+    private void Update()
+    {
+        PlayFootstepSound();
+    }
+
+
     public void PlayFootstepSound()
     {
-        if (footstepSounds.Length > 0)
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
-            int randomSoundIndex = Random.Range(0, footstepSounds.Length);
-            audioSource.clip = footstepSounds[randomSoundIndex];
-            audioSource.Play();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        { 
+         audioSource.Stop();
         }
     }
 }
