bdb4e45a3437a72810f6d29a50fe4454f234d855
peesasidech <129536390+Skyusehot007@users.noreply.github.com>
2023-11-07 19:02:24 +0700

    occlusion culling wall

---
+++ Assets/Script/Player/Trigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject prefabWithSpriteRenderer;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = prefabWithSpriteRenderer.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Color newColor = spriteRenderer.color;
+            newColor.a = 0.5f;
+            spriteRenderer.color = newColor;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Color newColor = spriteRenderer.color;
+            newColor.a = 1f;
+            spriteRenderer.color = newColor;
+        }
+    }
+}
