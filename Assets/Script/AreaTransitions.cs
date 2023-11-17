b1e0fc26d6a54c4494381f490668d623537455ef
ivamxx <terracatz19@gmail.com>
2023-11-17 10:21:21 +0700

    Canvas

---
+++ Assets/Script/AreaTransitions.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTransitions : MonoBehaviour
+{
+    private CameraController cam;
+
+    public Vector2 newMinPos;
+    public Vector2 newMaxPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            cam.minPosition = newMinPos;
+            cam.maxPosition = newMaxPos;
+        }
+    }
+}
