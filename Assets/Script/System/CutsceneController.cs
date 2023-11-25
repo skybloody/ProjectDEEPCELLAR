c9a24aa306da1a4f5b136c9caeb7e89684f484f6
peesasidech <35372937+skybloody@users.noreply.github.com>
2023-11-26 05:40:44 +0700

    Tom ไฟแตก

---
+++ Assets/Script/System/CutsceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneController : MonoBehaviour
+{
+    void StartCutscene()
+    {
+        // ทำ cutscene ที่นี่
+
+        // เมื่อ cutscene จบ
+        SceneManager.LoadScene("NextScene");
+    }
+    void EndCutscene()
+    {
+        // เมื่อ cutscene จบ
+        SceneManager.LoadScene("NextScene");
+    }
+}
